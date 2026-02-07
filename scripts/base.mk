e9d462773e7ceb8d678ef3bf663d67f6b0608ef2
Alessandro Ros <aler9.dev@gmail.com>
2026-02-07 15:42:18 +0100

    upgrade Raspbian/Trixie key (#86)

--- scripts/base.mk
+++ scripts/base.mk
@@ -11,10 +11,10 @@ FROM --platform=linux/arm/v7 $(DEBIAN_BULLSEYE_IMAGE)
 
 RUN apt update \
 	&& apt install -y wget gpg \
-	&& echo "deb http://archive.raspbian.org/raspbian bullseye main rpi firmware" > /etc/apt/sources.list \
-	&& echo "deb http://archive.raspberrypi.org/debian bullseye main" > /etc/apt/sources.list.d/raspi.list \
 	&& wget -O- https://archive.raspbian.org/raspbian.public.key | gpg --dearmor -o /etc/apt/trusted.gpg.d/raspbian.gpg \
+	&& echo "deb http://archive.raspbian.org/raspbian bullseye main rpi firmware" > /etc/apt/sources.list \
 	&& wget -O- https://archive.raspberrypi.org/debian/raspberrypi.gpg.key | gpg --dearmor -o /etc/apt/trusted.gpg.d/raspberrypi.gpg \
+	&& echo "deb http://archive.raspberrypi.org/debian bullseye main" > /etc/apt/sources.list.d/raspi.list \
 	&& rm -rf /var/lib/apt/lists/*
 
 RUN apt update && apt install --reinstall -y \
@@ -32,8 +32,8 @@ FROM --platform=linux/arm64 $(DEBIAN_BULLSEYE_IMAGE)
 
 RUN apt update \
 	&& apt install -y wget gpg \
-	&& echo "deb http://archive.raspberrypi.org/debian bullseye main" > /etc/apt/sources.list.d/raspi.list \
 	&& wget -O- https://archive.raspberrypi.org/debian/raspberrypi.gpg.key | gpg --dearmor -o /etc/apt/trusted.gpg.d/raspberrypi.gpg \
+	&& echo "deb http://archive.raspberrypi.org/debian bullseye main" > /etc/apt/sources.list.d/raspi.list \
 	&& rm -rf /var/lib/apt/lists/*
 endef
 export DOCKERFILE_BASE_BULLSEYE_64
@@ -51,10 +51,10 @@ FROM --platform=linux/arm/v7 $(DEBIAN_BOOKWORM_IMAGE)
 
 RUN apt update \
 	&& apt install -y wget gpg \
-	&& echo "deb http://archive.raspbian.org/raspbian bookworm main rpi firmware" > /etc/apt/sources.list \
-	&& echo "deb http://archive.raspberrypi.org/debian bookworm main" > /etc/apt/sources.list.d/raspi.list \
 	&& wget -O- https://archive.raspbian.org/raspbian.public.key | gpg --dearmor -o /etc/apt/trusted.gpg.d/raspbian.gpg \
+	&& echo "deb http://archive.raspbian.org/raspbian bookworm main rpi firmware" > /etc/apt/sources.list \
 	&& wget -O- https://archive.raspberrypi.org/debian/raspberrypi.gpg.key | gpg --dearmor -o /etc/apt/trusted.gpg.d/raspberrypi.gpg \
+	&& echo "deb http://archive.raspberrypi.org/debian bookworm main" > /etc/apt/sources.list.d/raspi.list \
 	&& rm -rf /var/lib/apt/lists/*
 
 RUN apt update && apt install --reinstall -y \
@@ -72,8 +72,8 @@ FROM --platform=linux/arm64 $(DEBIAN_BOOKWORM_IMAGE)
 
 RUN apt update \
 	&& apt install -y wget gpg \
-	&& echo "deb http://archive.raspberrypi.org/debian bookworm main" > /etc/apt/sources.list.d/raspi.list \
 	&& wget -O- https://archive.raspberrypi.org/debian/raspberrypi.gpg.key | gpg --dearmor -o /etc/apt/trusted.gpg.d/raspberrypi.gpg \
+	&& echo "deb http://archive.raspberrypi.org/debian bookworm main" > /etc/apt/sources.list.d/raspi.list \
 	&& rm -rf /var/lib/apt/lists/*
 endef
 export DOCKERFILE_BASE_BOOKWORM_64
@@ -91,10 +91,18 @@ FROM --platform=linux/arm/v7 $(DEBIAN_TRIXIE_IMAGE)
 
 RUN apt update \
 	&& apt install -y wget gpg \
-	&& echo "deb http://archive.raspbian.org/raspbian trixie main rpi firmware" > /etc/apt/sources.list \
-	&& echo "deb http://archive.raspberrypi.org/debian trixie main" > /etc/apt/sources.list.d/raspi.list \
-	&& wget -O- https://archive.raspbian.org/raspbian.public.key | gpg --dearmor -o /etc/apt/trusted.gpg.d/raspbian.gpg \
-	&& wget -O- https://archive.raspberrypi.org/debian/raspberrypi.gpg.key | gpg --dearmor -o /etc/apt/trusted.gpg.d/raspberrypi.gpg \
+	&& wget -O /raspbian-archive-keyring.deb \
+	https://archive.raspbian.org/raspbian/pool/main/r/raspbian-archive-keyring/raspbian-archive-keyring_20120528.4_all.deb \
+	&& dpkg -i /raspbian-archive-keyring.deb \
+	&& rm /raspbian-archive-keyring.deb \
+	&& wget -O /etc/apt/sources.list.d/raspbian.sources \
+	https://github.com/RPi-Distro/pi-gen/raw/358f9785089fa9fce397b7c36de2d90a9ae9a50e/stage0/00-configure-apt/files/raspbian.sources \
+	&& sed -i "s/RELEASE/trixie/g" /etc/apt/sources.list.d/raspbian.sources \
+	&& wget -O /usr/share/keyrings/raspberrypi-archive-keyring.pgp \
+	https://github.com/RPi-Distro/pi-gen/raw/358f9785089fa9fce397b7c36de2d90a9ae9a50e/stage0/00-configure-apt/files/raspberrypi-archive-keyring.pgp \
+	&& wget -O /etc/apt/sources.list.d/raspi.sources \
+	https://github.com/RPi-Distro/pi-gen/raw/358f9785089fa9fce397b7c36de2d90a9ae9a50e/stage0/00-configure-apt/files/raspi.sources \
+	&& sed -i "s/RELEASE/trixie/g" /etc/apt/sources.list.d/raspi.sources \
 	&& rm -rf /var/lib/apt/lists/*
 
 RUN apt update && apt install --reinstall -y \
@@ -112,8 +120,11 @@ FROM --platform=linux/arm64 $(DEBIAN_TRIXIE_IMAGE)
 
 RUN apt update \
 	&& apt install -y wget gpg \
-	&& echo "deb http://archive.raspberrypi.org/debian trixie main" > /etc/apt/sources.list.d/raspi.list \
-	&& wget -O- https://archive.raspberrypi.org/debian/raspberrypi.gpg.key | gpg --dearmor -o /etc/apt/trusted.gpg.d/raspberrypi.gpg \
+	&& wget -O /usr/share/keyrings/raspberrypi-archive-keyring.pgp \
+	https://github.com/RPi-Distro/pi-gen/raw/358f9785089fa9fce397b7c36de2d90a9ae9a50e/stage0/00-configure-apt/files/raspberrypi-archive-keyring.pgp \
+	&& wget -O /etc/apt/sources.list.d/raspi.sources \
+	https://github.com/RPi-Distro/pi-gen/raw/358f9785089fa9fce397b7c36de2d90a9ae9a50e/stage0/00-configure-apt/files/raspi.sources \
+	&& sed -i "s/RELEASE/trixie/g" /etc/apt/sources.list.d/raspi.sources \
 	&& rm -rf /var/lib/apt/lists/*
 endef
 export DOCKERFILE_BASE_TRIXIE_64
