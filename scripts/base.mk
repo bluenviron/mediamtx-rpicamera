2611550aed37e4c57e411ee895a26ed5d1c2a1a8
Alessandro Ros <aler9.dev@gmail.com>
2026-03-21 00:36:00 +0100

    restore compatibility with armv6 devices (bluenviron/mediamtx#5590) (#95)

--- scripts/base.mk
+++ scripts/base.mk
@@ -10,17 +10,18 @@ FROM $(DEBIAN_BULLSEYE_IMAGE)
 # Raspbian libraries and compilers provide arm v6 compatibility.
 
 RUN apt update \
-	&& apt install -y wget gpg \
-	&& wget -O- https://archive.raspbian.org/raspbian.public.key | gpg --dearmor -o /etc/apt/trusted.gpg.d/raspbian.gpg \
-	&& echo "deb http://archive.raspbian.org/raspbian bullseye main rpi firmware" > /etc/apt/sources.list \
-	&& wget -O- https://archive.raspberrypi.org/debian/raspberrypi.gpg.key | gpg --dearmor -o /etc/apt/trusted.gpg.d/raspberrypi.gpg \
-	&& echo "deb http://archive.raspberrypi.org/debian bullseye main" > /etc/apt/sources.list.d/raspi.list \
-	&& rm -rf /var/lib/apt/lists/*
+    && apt install -y wget gpg \
+    && wget -O- https://archive.raspbian.org/raspbian.public.key | gpg --dearmor -o /etc/apt/trusted.gpg.d/raspbian.gpg \
+    && echo "deb http://archive.raspbian.org/raspbian bullseye main rpi firmware" > /etc/apt/sources.list \
+    && wget -O- https://archive.raspberrypi.org/debian/raspberrypi.gpg.key | gpg --dearmor -o /etc/apt/trusted.gpg.d/raspberrypi.gpg \
+    && echo "deb http://archive.raspberrypi.org/debian bullseye main" > /etc/apt/sources.list.d/raspi.list \
+    && rm -rf /var/lib/apt/lists/*
 
 RUN apt update && apt install --reinstall -y \
-	libc6 \
-	libstdc++6 \
-	&& rm -rf /var/lib/apt/lists/*
+    libc6 \
+    libc-bin \
+    libstdc++6 \
+    && rm -rf /var/lib/apt/lists/*
 endef
 export DOCKERFILE_BASE_BULLSEYE_32
 
@@ -31,10 +32,10 @@ define DOCKERFILE_BASE_BULLSEYE_64
 FROM $(DEBIAN_BULLSEYE_IMAGE)
 
 RUN apt update \
-	&& apt install -y wget gpg \
-	&& wget -O- https://archive.raspberrypi.org/debian/raspberrypi.gpg.key | gpg --dearmor -o /etc/apt/trusted.gpg.d/raspberrypi.gpg \
-	&& echo "deb http://archive.raspberrypi.org/debian bullseye main" > /etc/apt/sources.list.d/raspi.list \
-	&& rm -rf /var/lib/apt/lists/*
+    && apt install -y wget gpg \
+    && wget -O- https://archive.raspberrypi.org/debian/raspberrypi.gpg.key | gpg --dearmor -o /etc/apt/trusted.gpg.d/raspberrypi.gpg \
+    && echo "deb http://archive.raspberrypi.org/debian bullseye main" > /etc/apt/sources.list.d/raspi.list \
+    && rm -rf /var/lib/apt/lists/*
 endef
 export DOCKERFILE_BASE_BULLSEYE_64
 
@@ -50,17 +51,18 @@ FROM $(DEBIAN_BOOKWORM_IMAGE)
 # Raspbian libraries and compilers provide arm v6 compatibility.
 
 RUN apt update \
-	&& apt install -y wget gpg \
-	&& wget -O- https://archive.raspbian.org/raspbian.public.key | gpg --dearmor -o /etc/apt/trusted.gpg.d/raspbian.gpg \
-	&& echo "deb http://archive.raspbian.org/raspbian bookworm main rpi firmware" > /etc/apt/sources.list \
-	&& wget -O- https://archive.raspberrypi.org/debian/raspberrypi.gpg.key | gpg --dearmor -o /etc/apt/trusted.gpg.d/raspberrypi.gpg \
-	&& echo "deb http://archive.raspberrypi.org/debian bookworm main" > /etc/apt/sources.list.d/raspi.list \
-	&& rm -rf /var/lib/apt/lists/*
+    && apt install -y wget gpg \
+    && wget -O- https://archive.raspbian.org/raspbian.public.key | gpg --dearmor -o /etc/apt/trusted.gpg.d/raspbian.gpg \
+    && echo "deb http://archive.raspbian.org/raspbian bookworm main rpi firmware" > /etc/apt/sources.list \
+    && wget -O- https://archive.raspberrypi.org/debian/raspberrypi.gpg.key | gpg --dearmor -o /etc/apt/trusted.gpg.d/raspberrypi.gpg \
+    && echo "deb http://archive.raspberrypi.org/debian bookworm main" > /etc/apt/sources.list.d/raspi.list \
+    && rm -rf /var/lib/apt/lists/*
 
 RUN apt update && apt install --reinstall -y \
-	libc6 \
-	libstdc++6 \
-	&& rm -rf /var/lib/apt/lists/*
+    libc6 \
+    libc-bin \
+    libstdc++6 \
+    && rm -rf /var/lib/apt/lists/*
 endef
 export DOCKERFILE_BASE_BOOKWORM_32
 
@@ -71,10 +73,10 @@ define DOCKERFILE_BASE_BOOKWORM_64
 FROM $(DEBIAN_BOOKWORM_IMAGE)
 
 RUN apt update \
-	&& apt install -y wget gpg \
-	&& wget -O- https://archive.raspberrypi.org/debian/raspberrypi.gpg.key | gpg --dearmor -o /etc/apt/trusted.gpg.d/raspberrypi.gpg \
-	&& echo "deb http://archive.raspberrypi.org/debian bookworm main" > /etc/apt/sources.list.d/raspi.list \
-	&& rm -rf /var/lib/apt/lists/*
+    && apt install -y wget gpg \
+    && wget -O- https://archive.raspberrypi.org/debian/raspberrypi.gpg.key | gpg --dearmor -o /etc/apt/trusted.gpg.d/raspberrypi.gpg \
+    && echo "deb http://archive.raspberrypi.org/debian bookworm main" > /etc/apt/sources.list.d/raspi.list \
+    && rm -rf /var/lib/apt/lists/*
 endef
 export DOCKERFILE_BASE_BOOKWORM_64
 
@@ -90,25 +92,26 @@ FROM $(DEBIAN_TRIXIE_IMAGE)
 # Raspbian libraries and compilers provide arm v6 compatibility.
 
 RUN apt update \
-	&& apt install -y wget gpg \
-	&& wget -O /raspbian-archive-keyring.deb \
-	https://archive.raspbian.org/raspbian/pool/main/r/raspbian-archive-keyring/raspbian-archive-keyring_20120528.4_all.deb \
-	&& dpkg -i /raspbian-archive-keyring.deb \
-	&& rm /raspbian-archive-keyring.deb \
-	&& wget -O /etc/apt/sources.list.d/raspbian.sources \
-	https://github.com/RPi-Distro/pi-gen/raw/358f9785089fa9fce397b7c36de2d90a9ae9a50e/stage0/00-configure-apt/files/raspbian.sources \
-	&& sed -i "s/RELEASE/trixie/g" /etc/apt/sources.list.d/raspbian.sources \
-	&& wget -O /usr/share/keyrings/raspberrypi-archive-keyring.pgp \
-	https://github.com/RPi-Distro/pi-gen/raw/358f9785089fa9fce397b7c36de2d90a9ae9a50e/stage0/00-configure-apt/files/raspberrypi-archive-keyring.pgp \
-	&& wget -O /etc/apt/sources.list.d/raspi.sources \
-	https://github.com/RPi-Distro/pi-gen/raw/358f9785089fa9fce397b7c36de2d90a9ae9a50e/stage0/00-configure-apt/files/raspi.sources \
-	&& sed -i "s/RELEASE/trixie/g" /etc/apt/sources.list.d/raspi.sources \
-	&& rm -rf /var/lib/apt/lists/*
+    && apt install -y wget gpg \
+    && wget -O /raspbian-archive-keyring.deb \
+    https://archive.raspbian.org/raspbian/pool/main/r/raspbian-archive-keyring/raspbian-archive-keyring_20120528.4_all.deb \
+    && dpkg -i /raspbian-archive-keyring.deb \
+    && rm /raspbian-archive-keyring.deb \
+    && wget -O /etc/apt/sources.list.d/raspbian.sources \
+    https://github.com/RPi-Distro/pi-gen/raw/358f9785089fa9fce397b7c36de2d90a9ae9a50e/stage0/00-configure-apt/files/raspbian.sources \
+    && sed -i "s/RELEASE/trixie/g" /etc/apt/sources.list.d/raspbian.sources \
+    && wget -O /usr/share/keyrings/raspberrypi-archive-keyring.pgp \
+    https://github.com/RPi-Distro/pi-gen/raw/358f9785089fa9fce397b7c36de2d90a9ae9a50e/stage0/00-configure-apt/files/raspberrypi-archive-keyring.pgp \
+    && wget -O /etc/apt/sources.list.d/raspi.sources \
+    https://github.com/RPi-Distro/pi-gen/raw/358f9785089fa9fce397b7c36de2d90a9ae9a50e/stage0/00-configure-apt/files/raspi.sources \
+    && sed -i "s/RELEASE/trixie/g" /etc/apt/sources.list.d/raspi.sources \
+    && rm -rf /var/lib/apt/lists/*
 
 RUN apt update && apt install --reinstall -y \
-	libc6 \
-	libstdc++6 \
-	&& rm -rf /var/lib/apt/lists/*
+    libc6 \
+    libc-bin \
+    libstdc++6 \
+    && rm -rf /var/lib/apt/lists/*
 endef
 export DOCKERFILE_BASE_TRIXIE_32
 
@@ -119,13 +122,13 @@ define DOCKERFILE_BASE_TRIXIE_64
 FROM $(DEBIAN_TRIXIE_IMAGE)
 
 RUN apt update \
-	&& apt install -y wget gpg \
-	&& wget -O /usr/share/keyrings/raspberrypi-archive-keyring.pgp \
-	https://github.com/RPi-Distro/pi-gen/raw/358f9785089fa9fce397b7c36de2d90a9ae9a50e/stage0/00-configure-apt/files/raspberrypi-archive-keyring.pgp \
-	&& wget -O /etc/apt/sources.list.d/raspi.sources \
-	https://github.com/RPi-Distro/pi-gen/raw/358f9785089fa9fce397b7c36de2d90a9ae9a50e/stage0/00-configure-apt/files/raspi.sources \
-	&& sed -i "s/RELEASE/trixie/g" /etc/apt/sources.list.d/raspi.sources \
-	&& rm -rf /var/lib/apt/lists/*
+    && apt install -y wget gpg \
+    && wget -O /usr/share/keyrings/raspberrypi-archive-keyring.pgp \
+    https://github.com/RPi-Distro/pi-gen/raw/358f9785089fa9fce397b7c36de2d90a9ae9a50e/stage0/00-configure-apt/files/raspberrypi-archive-keyring.pgp \
+    && wget -O /etc/apt/sources.list.d/raspi.sources \
+    https://github.com/RPi-Distro/pi-gen/raw/358f9785089fa9fce397b7c36de2d90a9ae9a50e/stage0/00-configure-apt/files/raspi.sources \
+    && sed -i "s/RELEASE/trixie/g" /etc/apt/sources.list.d/raspi.sources \
+    && rm -rf /var/lib/apt/lists/*
 endef
 export DOCKERFILE_BASE_TRIXIE_64
 
