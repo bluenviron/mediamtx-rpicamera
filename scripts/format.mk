d405998e817d53bcdbbda7ecf5d05e938c86949d
aler9 <46489434+aler9@users.noreply.github.com>
2026-06-25 15:20:40 +0200

    fix clang-format path

--- scripts/format.mk
+++ scripts/format.mk
@@ -6,4 +6,4 @@ export DOCKERFILE_FORMAT
 
 format:
 	echo "$$DOCKERFILE_FORMAT" | docker build . -f - -t format
-	docker run --rm -v $(shell pwd):/s -w /s format sh -c "clang-format -i *.cpp *.c *.h"
+	docker run --rm -v $(shell pwd):/s -w /s format sh -c "/usr/lib/llvm21/bin/clang-format -i *.cpp *.c *.h"
